74aacdd9dfd7eabb97d798f449193bb55660cc55
Avanish Gupta <avanishcodes@gmail.com>
2023-01-09 13:36:01 +0530

    Add configuration for debugging in container

--- src/main.py
+++ src/main.py
@@ -1,13 +1,25 @@
+import redis
+import debugpy
 from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
+
+r = redis.Redis(host='redis', port=6379)
+
+# debugpy.listen("0.0.0.0", 5678)
+# debugpy.wait_for_client()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "Ayodhya"}
+
+
+@app.get("/hits")
+def read_root():
+    r.incr('hits') # Breakpoint line
+    return {"hits": r.get('hits')}
 
 
 @app.get("/items/{item_id}")
